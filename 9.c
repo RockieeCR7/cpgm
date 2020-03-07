dcf837df2045effe888d34453ecf7499f8855738
RockieeCR7 <61724287+RockieeCR7@users.noreply.github.com>
2020-03-07 12:41:20 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{int a,b,c;
+printf(" enter the value of a and b\n");
+scanf("%d%d",&a,&b);
+printf(" befor swapping \n");
+printf("a=%db=%d\n",a,b);
+c=a;
+a=b;
+b=c;
+printf("after swapping\n");
+printf("a=%d b=%d\n",a,b);
+}
